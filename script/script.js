2bbf00ef843b544b761070e046dd759b1bad51c9
Ahmed <a.khattab2021@gmail.com>
2023-10-31 12:54:26 +0100

    start the project Html+css+Js pages

---
+++ script/script.js
@@ -0,0 +1,63 @@
+
+
+// THE ACCOUNT OBJECT SHOULD HAVE THE FOLLWOING PROPERTIES:
+
+// expenses: an array that should hold all of your expenses, can be empty from start
+
+// income: an array that should hold all of your income, can be empty from start
+// add Expenses: a function that vill add an expense to the expenses array
+
+// add Income: a function that vill add an income to the income array
+
+// list All Expenses: a function that should list all the expenses in the expenses array
+// get Summary: a function that should summarize your total balances. It should show your total
+// income, your total expenses and also calculate your current balances(income - expenses).
+
+
+
+
+
+
+// let expenses = window.prompt();
+
+
+
+
+// let income = [25000+12000]
+// let income2 = [12000]
+// document.write(Math.income);
+
+
+
+
+
+
+// TODO
+//* 1. put the name property in the object as the datatype string DONE
+//* 2. create the menu and the different choices for the user DONE
+//* 3. take the input from the user and store it (save it) in a variable DONE
+//* 4. use the stored/saved variable to display the correct menu choice
+
+
+// OBJECT
+
+// const account = {
+//   // name: the account holders name, should be a string
+//   name: "Helena",};
+
+
+//* FUNCTION
+// function menu() {
+//   // the function menu() should only be responsible to show the different choices
+//   // for the user.
+//   const choice = parseFloat(
+//     prompt(
+//       "Please select from the menu:\n1) Add income\n2) Add expense\n3)List all expenses\n4) See total balance"
+//     )
+//   );
+
+//   console.log("Choice: " + choice);
+// }
+
+// menu();
+// document.write(expenses)
